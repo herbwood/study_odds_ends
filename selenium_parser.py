d706df60b20affffe3592cf04a3a61d40aab7c0b
herbwood <herbwood27@gmail.com>
2018-09-22 22:37:57 +0900

    made selenium parser and get purchase history of naver pay

---
+++ selenium_parser.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+driver = webdriver.Chrome(r'C:\Users\default.DESKTOP-S5Q9GAA\Downloads\chromedriver_win32\chromedriver')
+
+driver.implicitly_wait(3)
+
+driver.get('https://nid.naver.com/nidlogin.login')
+
+driver.find_element_by_name('id').send_keys('jhkwon94')
+driver.find_element_by_name('pw').send_keys('dirchdmltnv1787')
+driver.find_element_by_xpath('//*[@id="frmNIDLogin"]/fieldset/input').click()
+
+driver.get('https://order.pay.naver.com/home')
+html = driver.page_source
+soup = BeautifulSoup(html, 'html.parser')
+notices = soup.select('div.p_inr > div.p_info > a > span')
+
+for n in notices:
+    print(n.text.strip())
