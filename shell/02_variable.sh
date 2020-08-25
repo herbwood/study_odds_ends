4957b123e204aecd8dd2d77ca79f59630d471d0d
herbwood <herbwood27@gmail.com>
2020-08-26 03:00:56 +0900

    studied on shell variables and comments

---
+++ shell/02_variable.sh
@@ -0,0 +1,17 @@
+
+#! /bin/bash
+# this is comment
+
+echo "Hello World"
+
+# 환경변수
+echo Our shell name is $ZSH
+echo Our home directory is $HOME
+echo Our current working directory is $PWD
+
+# 사용자 정의 변수
+# 띄어쓰기 조심!
+name=Mark
+val=10
+echo The name in $name
+echo value $val
