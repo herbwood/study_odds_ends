bfc78c73a83667a5694691dff9e66a4e108be713
herbwood <herbwood27@gmail.com>
2020-09-01 20:24:48 +0900

    learned float operations

--- shell/09_arithmetic_operations.sh
+++ shell/09_arithmetic_operations.sh
@@ -13,3 +13,27 @@ echo $(( num1 % num2 ))
 # expr : * 할 때 주의!
 echo $(expr $num1 + $num2)
 echo $(expr $num1 \* $num2)
+
+
+## floating point math operations in bash
+
+num1=20.5
+num2=5
+
+echo "20.5 + 5" | bc
+echo "20.5 - 5" | bc
+echo "20.5 * 5" | bc
+
+# scale을 지정해줘야 함!!
+echo "scale=2;20.5 / 5" | bc
+
+# 변수를 사용해야하는 경우!
+echo "$num1 + $num2" | bc
+
+# 제곱
+# -l : sqrt를 불러오는 라이브러리
+num=27
+
+echo "scale=2;sqrt($num)" | bc -l
+echo "scale=2;3^3" | bc -l
+
