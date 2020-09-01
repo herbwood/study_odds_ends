3caf2e61abdbdf140ecc337b259c0737aac80b1e
herbwood <herbwood27@gmail.com>
2020-09-01 20:44:20 +0900

    learned if, while statement

--- java/Statement.java
+++ java/Statement.java
@@ -1,6 +1,8 @@
 
 public class Statement {
-
+	
+	final static int N=30;
+	final static int A=15;
 	public static void main(String[] args) {
 		// 중복 연산자
 		int i = 20;
@@ -41,6 +43,87 @@ public class Statement {
 			System.out.println("F입니다");
 		}
 		
+		
+		String c = "Man";
+		int b = 0;
+		
+		// 자바는 String은 비교할 때 equal()을 사용합니다
+		// 그 이유는 String은 다른 자료형과 다른 문자열 자료형이기 때문입니다
+		
+		if (c.equals("Man"))
+		{
+			System.out.println("남자입니다");
+		}
+		
+		else
+		{
+			System.out.println("남자가 아닙니다");
+		}
+		
+		if(b==3)
+		{
+			System.out.println("b는 3입니다.");
+		}
+		
+		else
+		{
+			System.out.println("3이 아닙니다.");
+		}
+		
+		// String에서 대소문자 구분 X
+		if (c.equalsIgnoreCase("man"))
+		{
+			System.out.println("참입니다");
+		}
+		
+		// 연산자 활용하기
+		if (c.equalsIgnoreCase("man") && b == 0)
+		{
+			System.out.println("참입니다");
+		}
+		else
+		{
+			System.out.println("거짓입니다");
+		}
+		
+		
+		// while문을 통해 1~1000까지의 합을 출력
+		int num = 1, sum = 0;
+		while (num <= 1000)
+		{
+			sum += num++;
+		}
+		System.out.println("1부터 1000까지의 합은 " + sum + "입니다");
+		
+		
+		// for문을 사용하여 삼각형을 출력
+		for (int z=N; z > 0; z--)
+		{
+			for (int j=z; j > 0; j--)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		
+		// for문을 통해 원을 출력하기
+		for (int q=-A; q <= A; q++)
+		{
+			for (int w=-A; w <= A; w++)
+			{
+				if (q * q + w * w < A * A)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
 	}
 
 }
+
