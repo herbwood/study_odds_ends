a45129b061f46b287c636fd445aef9efc4af0a8d
herbwood <herbwood27@gmail.com>
2020-09-03 16:05:01 +0900

    learned regular expression

--- shell/10_case_statement.sh
+++ shell/10_case_statement.sh
@@ -14,3 +14,25 @@ case $vehicle in
 	* )
 		echo "Unknown vehicle";;
 esac
+
+
+## example
+
+echo -e "Enter some character : \c"
+read value
+
+case $value in 
+	[a-z] )
+		echo "User entered $value a to z" ;;
+	[A-Z] )
+		echo "User entered $value A to Z" ;;
+	[0-9] )
+		echo "User entered $value 0 to 9" ;;
+	#? : 길이가 1인 character
+	? )
+		echo "User entered $value special character" ;;
+
+	# * : 길이가 1개 이상인 character
+	* )
+		echo "Unknwon input" ;;
+esac
